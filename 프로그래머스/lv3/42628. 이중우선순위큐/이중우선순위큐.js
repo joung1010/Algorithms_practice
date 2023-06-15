1b32626319be5942534f6d66f59b51f672c13ab1
mason park <joung4342@gmail.com>
2023-06-15 09:00:54 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.js
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.js
@@ -1,19 +1,19 @@
 function solution(operations) {
     const queue = [];
-    while (operations.length) {
-        const operation = operations.shift().split(' ');
-        if (operation.some(word => word === 'I')) {
-            queue.push(parseInt(operation[1]));
+    operations.forEach(operation => {
+        const command = operation.split(' ');
+        if (command[0] === 'I') {
+            queue.push(parseInt(command[1]));
             queue.sort((a, b) => b - a);
-            continue;
+            return;
         }
-        if (!queue.length) continue;
+        if (!queue.length) return;
 
-        if (operation[1] === '1') {
+        if (command[1] === '1') {
             queue.shift();
         } else {
             queue.pop();
         }
-    }
+    })
     return queue.length ? [queue[0], queue[queue.length-1]] : [0, 0];
 }
