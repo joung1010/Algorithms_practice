c021c43b1f593987ee76833b380009a5395595b9
mason park <joung4342@gmail.com>
2023-06-20 21:50:01 +0900

    [level 3] Title: 여행경로, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.js
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.js
@@ -1,27 +1,26 @@
 function solution(tickets) {
-    tickets.sort();
+    const myTickets = tickets.map((t, i) => [...t, i]);
+    const queue = [];
     const used = Array(tickets.length).fill(0);
-    let path = [];
-    const makePath = (curr, visited) => {
-        const queue = [];
-        queue.push({from: curr, visited});
-        while (queue.length) {
-            const {from, visited} = queue.shift();
-            if (visited.length === tickets.length + 1) {
-                path.push(visited);
-                return;
-            }
+    const visited = ["ICN"];
 
-            for (let i = 0; i < tickets.length; i++) {
-                if (used[i]) continue;
-                if (tickets[i][0] === from) {
-                    used[i] = true;
-                    makePath(tickets[i][1], [...visited, tickets[i][1]]);
-                    used[i] = false;
-                }
-            }
-        }
-    };
-    makePath("ICN", ["ICN"]);
-    return path[0];
+    queue.push("ICN");
+    while (queue.length) {
+        const dep = queue.shift();
+        const ticket = myTickets
+            .filter(t => !used[t[2]] && t[0] === dep) // 사용되지 않는 티켓과 목표 출발지가 동일한 티켓
+            .filter((t, _, array) => { // 티켓이 2개이상일때 티켓의 목적지가 남은 티켓의 출발지로 존재하는 체크
+                if (array.length === 1) return true;
+                const [src, des] = t;
+                return myTickets.some((i => !used[i[2]] && i[0] === des))
+            })
+            .sort((a, b) => a[1].localeCompare(b[1])) // 2개 이상일때 알파벳을 기준으로 정렬
+            .find((_, i) => i === 0); // 1건 추출
+        if (!ticket) continue;
+
+        used[ticket[2]] = true;
+        queue.push(ticket[1]);
+        visited.push(ticket[1]);
+    }
+    return visited;
 }
