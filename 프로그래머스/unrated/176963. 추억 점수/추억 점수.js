7c612ed8fe6f81fcc15dec2d9d0e51ad2a39efce
mason park <joung4342@gmail.com>
2023-06-22 16:20:36 +0900

    [unrated] Title: 추억 점수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
@@ -1,12 +1,5 @@
+// ?? 문법
+// 거짓의 판단을 유연하게 판단. 그냥 심플하게 값이 있고 없고로 판단
 function solution(name, yearning, photo) {
-    const cost = new Map();
-    name.forEach((n,i) => {
-        cost.set(n, yearning[i]);
-    });
-    
-   return  photo.map(people => {
-        let score = 0;
-        people.filter(p => cost.has(p)).forEach(person => score += cost.get(person));
-        return score;
-    });
+    return photo.map((v)=> v.reduce((a, c)=> a += yearning[name.indexOf(c)] ?? 0, 0))
 }
