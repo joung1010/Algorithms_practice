2a442569f0dd4c8786454acbf90159e155298b2e
mason park <joung4342@gmail.com>
2023-06-30 09:25:32 +0900

    [level 2] Title: 피보나치 수, Time: 4.72 ms, Memory: 38.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,7 @@
+function solution(n) {
+    const fibonacci = [0, 1];
+    for (let i = 2; i <= n; i++) {
+        fibonacci[i] = (fibonacci[i - 1] + fibonacci[i - 2]) % 1234567;
+    }
+    return fibonacci[n];
+}
