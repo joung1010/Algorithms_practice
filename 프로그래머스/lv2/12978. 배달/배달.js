a0fea109ae8da487d70f86dfe544b7381f1feb2e
mason park <joung4342@gmail.com>
2023-07-03 19:28:46 +0900

    [level 2] Title: 배달, Time: 0.42 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12978. 배달/배달.js
+++ 프로그래머스/lv2/12978. 배달/배달.js
@@ -1,26 +1,8 @@
 function solution(N, road, K) {
-    const delivery = Array(N + 1).fill(Number.POSITIVE_INFINITY);
-    const cost = Array.from({length: N + 1}, () => []);
-    const queue = [];
+    var answer = 0;
 
-    road.forEach((r, i) => {
-        const [from, to, time] = r;
-        cost[from].push({to, time});
-        cost[to].push({to: from, time});
-    })
-    delivery[1] = 0;
-    queue.push(1);
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    console.log('Hello Javascript')
 
-    while (queue.length) {
-        const node = queue.shift();
-
-        cost[node].forEach((c, i) => {
-            if (delivery[c.to] > c.time + delivery[node]) {
-                delivery[c.to] = c.time + delivery[node];
-                queue.push(c.to);
-            }
-
-        });
-    }
-    return delivery.filter(v => v<=K).length;
+    return answer;
 }
