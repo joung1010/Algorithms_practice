3416fa7e1e7a87ed6ece8da85fc32430ee743c4a
mason park <joung4342@gmail.com>
2023-07-04 21:19:40 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
@@ -1,30 +1,10 @@
 function solution(n, a, b) {
-    let tournament = [];
-    let cnt = 1;
-
-    for (let i = 2; i <= n; i += 2) {
-        tournament.push([i - 1, i]);
-    }
-    while (tournament.length > 1) {
-        if (tournament.some(competition => competition.includes(a) && competition.includes(b))) return cnt;
-        const round = [];
-        const winners = tournament.map(competition => {
-            const [c1, c2] = competition;
-            if (c1 === a || c1 === b) {
-                return c1
-            } else if (c2 === a || c2 === b) {
-                return c2;
-            } else {
-                return c1;
-            }
-        });
-        for (let i = 1; i <= winners.length; i += 2) {
-            round.push([winners[i - 1], winners[i]]);
-        }
-
-        cnt++;
-        tournament = round;
+    let answer = 0;
+    while(a !== b) {
+        a = Math.ceil(a/2);
+        b = Math.ceil(b/2);
+        answer++;
     }
 
-    return cnt;
+    return answer;
 }
