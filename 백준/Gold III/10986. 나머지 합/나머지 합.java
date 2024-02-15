63dc62b5242bda2683af6af4cb135439a70ce9e0
mason park <joung4342@gmail.com>
2024-02-15 18:08:03 +0900

    [Gold III] Title: 나머지 합, Time: 1840 ms, Memory: 328880 KB -BaekjoonHub

---
+++ 백준/Gold III/10986. 나머지 합/나머지 합.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        long res = 0L;
+
+        long[] sumArr = new long[N];
+        sumArr[0] = sc.nextInt();
+        for (int i = 1; i < N; i++) {
+            sumArr[i] = sumArr[i - 1] + sc.nextInt();
+        }
+
+        long[] remainCntArr = new long[M];
+        for (int i = 0; i < N; i++) {
+            int remain = (int) (sumArr[i] % M);
+
+            if (remain == 0) res++;
+
+            remainCntArr[remain]++;
+        }
+
+        for (int i = 0; i < M; i++) {
+            if (remainCntArr[i] > 1) {
+                res += (remainCntArr[i] * (remainCntArr[i] - 1)) / 2;
+            }
+        }
+        System.out.println(res);
+
+    }
+}
