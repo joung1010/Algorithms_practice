4ef29f7c2ed87c44cea8d5430d196ffad4852327
mason park <joung4342@gmail.com>
2024-02-19 19:50:13 +0900

    [Silver IV] Title: 주몽, Time: 476 ms, Memory: 34272 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[] uniqueNum = new int[N];
+        for (int i = 0; i < N; i++) {
+            uniqueNum[i] = sc.nextInt();
+        }
+        Arrays.sort(uniqueNum);
+
+
+        int strIdx = 0;
+        int endIdx = N - 1;
+        int count = 0;
+
+        while (strIdx < endIdx) {
+            if (uniqueNum[strIdx] + uniqueNum[endIdx] == M) {
+                strIdx++;
+                endIdx--;
+                count++;
+            } else if (uniqueNum[strIdx] + uniqueNum[endIdx] > M) {
+                endIdx--;
+            } else {
+                strIdx++;
+            }
+        }
+        System.out.println(count);
+    }
+}
