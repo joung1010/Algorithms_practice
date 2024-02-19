a561939a356d8b0385b11f71d36a0627ec7e31cb
mason park <joung4342@gmail.com>
2024-02-19 19:32:14 +0900

    [Silver V] Title: 수들의 합 5, Time: 256 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
+       int N = sc.nextInt();
+
+       int count = 1;
+       int strIdx = 1;
+       int endIdx = 1;
+       int sum = 1;
+
+        while (endIdx != N) {
+            if (sum == N) {
+                endIdx++;
+                count++;
+                sum += endIdx;
+            } else if (sum > N) {
+                sum -= strIdx;
+                strIdx++;
+            } else {
+                endIdx++;
+                sum += endIdx;
+            }
+        }
+        System.out.println(count);
+    }
+}
