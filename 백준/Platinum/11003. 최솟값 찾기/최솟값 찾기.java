32014dfb19d8e862ef84f380a9cbea6db92a9586
mason park <joung4342@gmail.com>
2024-03-06 20:29:03 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2472 ms, Memory: 618628 KB -BaekjoonHub

---
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -0,0 +1,72 @@
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        // 값을 보관 및 정렬 해 줄 deque
+        Deque<Node> mydeque = new LinkedList<>();
+        for (int i = 0; i < N; i++) {
+            int now = Integer.parseInt(st.nextToken());
+
+            // 덱이 비거나, 덱 마지막에 현재 값(now)보다 큰 값 지우기
+            while (!mydeque.isEmpty() && mydeque.getLast().getValue() > now) {
+                mydeque.removeLast();
+            }
+
+            // 덱 마지막에 값 삽입
+            mydeque.addLast(new Node(i, now));
+
+            // 덱의 첫번째 인덱스에서 범위를 벗어난 값 지우기
+            while (mydeque.getFirst().getIndex() <= i - L) {
+                mydeque.removeFirst();
+            }
+
+            // 최소값 출력
+            bw.write(mydeque.getFirst().getValue() + " ");
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    static class Node {
+        private int index;
+        private int value;
+
+        public Node(int index, int value) {
+            this.index = index;
+            this.value = value;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public void setIndex(int index) {
+            this.index = index;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+    }
+}
