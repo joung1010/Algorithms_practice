d0db0908aeb63e6ebba065803a7b9f33851c762c
mason park <joung4342@gmail.com>
2024-03-11 20:13:39 +0900

    [Silver II] Title: 스택 수열, Time: 1076 ms, Memory: 107856 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int maxNum = sc.nextInt();
+        int[] numArr = new int[maxNum];
+        IntStream.range(0, maxNum)
+                .forEach(i -> numArr[i] = sc.nextInt());
+
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder printer = new StringBuilder();
+        int standard = 1;
+        boolean result = true;
+        for (int curNum : numArr) {
+            if (curNum >= standard) {
+                while (curNum >= standard) {
+                    stack.push(standard++);
+                    printer.append("+\n");
+                }
+            }
+            if (stack.isEmpty() || stack.peek() != curNum) {
+                System.out.println("NO");
+                result = false;
+                break;
+            }
+            stack.pop();
+            printer.append("-\n");
+        }
+        if (result) System.out.println(printer.toString());
+    }
+}
