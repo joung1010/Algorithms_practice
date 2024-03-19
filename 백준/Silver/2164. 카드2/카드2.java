f1524fb469132cfda2a328f903b82330b281aae8
mason park <joung4342@gmail.com>
2024-03-19 19:21:19 +0900

    [Silver IV] Title: 카드2, Time: 280 ms, Memory: 49056 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        Deque<Integer> queue = new LinkedList<>();
+        IntStream.rangeClosed(1, N)
+                .forEach(queue::add);
+
+        while (queue.size() > 1) {
+            queue.poll();
+            queue.add(queue.poll());
+        }
+        System.out.println(queue.poll());
+    }
+}
