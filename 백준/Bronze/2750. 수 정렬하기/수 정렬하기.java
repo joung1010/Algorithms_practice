55b3d25575254d9c2419cc47ed3e24bc449d52b4
mason park <joung4342@gmail.com>
2024-03-25 18:58:56 +0900

    [Bronze II] Title: 수 정렬하기, Time: 356 ms, Memory: 22552 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        List<Integer> numberList = IntStream.range(0, N)
+                .mapToObj(i -> sc.nextInt())
+                .collect(Collectors.toList());
+
+        numberList.stream()
+                .sorted()
+                .forEach(System.out::println);
+    }
+}
