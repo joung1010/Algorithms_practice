65acbe36b75951de2efa4ba5a019f06d30fa09c6
mason park <joung4342@gmail.com>
2024-03-25 20:00:34 +0900

    [Gold II] Title: 버블 소트, Time: 1732 ms, Memory: 81008 KB -BaekjoonHub

---
+++ 백준/Gold/1377. 버블 소트/버블 소트.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+
+        Node[] nodeArray = new Node[N];
+        for (int i = 0; i < N; i++) {
+            nodeArray[i] = new Node(Integer.parseInt(bf.readLine()), i);
+        }
+
+        Arrays.sort(nodeArray);
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            if (result < nodeArray[i].index - i) {
+                result = nodeArray[i].index - i;
+            }
+        }
+        System.out.println(result + 1);
+    }
+
+    static class Node implements Comparable<Node> {
+        int value;
+        int index;
+
+        public Node(int value, int index) {
+            this.value = value;
+            this.index = index;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.value - o.value;
+        }
+    }
+}
