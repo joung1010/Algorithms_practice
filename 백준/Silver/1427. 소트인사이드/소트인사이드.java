1f471f1dffba2154c0c11031c70c3fea277001af
mason park <joung4342@gmail.com>
2024-03-26 19:01:09 +0900

    [Silver V] Title: 소트인사이드, Time: 216 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
+        String number = sc.next();
+
+        String sortedNum = Arrays.asList(String.valueOf(number).split(""))
+                .stream()
+                .map(Integer::parseInt)
+                .sorted(Comparator.reverseOrder())
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+        System.out.println(sortedNum);
+    }
+}
