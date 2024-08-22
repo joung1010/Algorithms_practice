15839b5cd258d4d2c6b74d01d0835ae602240bae
mason park <joung4342@gmail.com>
2024-08-22 20:37:23 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1204 ms, Memory: 56132 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final int N = Integer.parseInt(br.readLine());
+
+        List<int[]> positionList = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int[] arr = new int[2];
+            arr[0] = Integer.parseInt(st.nextToken());
+            arr[1] = Integer.parseInt(st.nextToken());
+            positionList.add(arr);
+        }
+
+        Collections.sort(positionList
+                , (o1, o2) -> {
+                    if (o1[0] == o2[0]) {
+                        return o1[1] - o2[1];
+                    } else {
+                        return o1[0] - o2[0];
+                    }
+                });
+
+        for (int[] pos : positionList) {
+            System.out.println(pos[0] + " " + pos[1]);
+        }
+    }
+}
